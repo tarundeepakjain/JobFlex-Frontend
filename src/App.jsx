e45a10eb907516f2093f0de434e2a39aaad2a86f
Namankumar608x <nmukati3000@gmail.com>
2026-01-29 13:20:22 +0530

    react setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-center pt-10 text-2xl font-semibold">
+        JobFlex Frontend
+      </h1>
+    </div>
+  )
+}
+
+
+
